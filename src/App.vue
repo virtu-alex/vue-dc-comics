529358c888a89627703421f88303fbb44482a345
virtu-alex <alessio.cordari@gmail.com>
2022-07-22 13:56:53 +0200

    Components + image porting

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+*/Descrizione:
+Create un nuovo progetto utilizzando Vue CLI e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+Bonus:
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+Questo bonus nasconde un'insidia legata alla renderizzazione delle immagini che potrebbe farvi venire qualche grattacapo! Cercate di trovare una soluzione da veri programmatori e proprio in caso di emergenza, aprite un ticket!
+/*
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
