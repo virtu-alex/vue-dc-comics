3959232a229b74505d41b58dcc734af72b4fc181
virtu-alex <alessio.cordari@gmail.com>
2022-07-23 11:16:11 +0200

    css Loader options url to false

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      // @/ is an alias to src/
+      // so this assumes you have a file named `src/variables.sass`
+      // Note: this option is named as "prependData" in sass-loader v8
+      css: {
+
+          url: false,
+      },
+    },
+  }
 })
